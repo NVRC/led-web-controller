8c79ea87fcf9d9490dea36584548eecfb3455193
NVRC <nathaniel.charlebois@gmail.com>
2017-09-23 17:56:29 -0400

    More testing

--- php/leds.php
+++ php/leds.php
@@ -2,7 +2,7 @@
 
     //Node.js would be a good alternative to this tedious php workflow
 
-    $command = escapeshellcmd('sudo python /var/www/led-web-controller/python/led_output.py  123');
+    $command = escapeshellcmd('python /var/www/led-web-controller/python/led_output.py  123');
     $output = shell_exec($command);
     echo $output;
 
