b20da76d73dce7f649e3d6adeb67815db93ecd96
NVRC <nathaniel.charlebois@gmail.com>
2017-09-23 16:20:08 -0400

    Improve escapeshellarg

--- php/leds.php
+++ php/leds.php
@@ -8,7 +8,7 @@ for($i = 0; $i < $NUM_LEDS; $i++){
 
 
 
-$command = escapeshellcmd('/var/www/led-web-controller/python/led_output.py '.$colorArray);
+$command = escapeshellcmd('/var/www/led-web-controller/python/ led_output.py '.escapeshellarg($colorArray));
 $output = shell_exec($command);
 echo $output;
 
