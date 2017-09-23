4ee04e0abc2d265c9738d970316aee40eeb9e9ad
NVRC <nathaniel.charlebois@gmail.com>
2017-09-23 18:24:20 -0400

    figuring out php

--- php/leds.php
+++ php/leds.php
@@ -2,14 +2,17 @@
 
     //Node.js would be a good alternative to this tedious php workflow
 
-    $command = escapeshellcmd('python /var/www/led-web-controller/python/led_output.py  123');
+
+
+
+    $colorArray = array();
+    $NUM_LEDS = 60;
+
+    $command = escapeshellcmd('python /var/www/led-web-controller/python/led_output.py args');
     $output = shell_exec($command);
     echo $output;
 
     /*
-    $colorArray = array();
-    $NUM_LEDS = 60;
-
     for($i = 0; $i < $NUM_LEDS; $i++){
         $colorArray[] = $_POST[$i];
     }
--- python/led_output.py
+++ python/led_output.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python
 
 import sys
-print(sys.argv[1:])
-f = open('sharedmem.txt','w')
-f.write(sys.argv[1:][0])
-f.close()
+args = sys.argv[1:];
