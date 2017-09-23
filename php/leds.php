bea2f99a104630412c372b8f3a5d6d4c2a4a0ef1
NVRC <nathaniel.charlebois@gmail.com>
2017-09-23 15:47:17 -0400

    Added sharedem tester

--- php/leds.php
+++ php/leds.php
@@ -7,7 +7,11 @@ for($i = 0; $i < NUM_LEDS; i++){
 }
 
 
+
 $command = escapeshellcmd('/var/www/led-web-controller/python/led_output.py '.colorArray);
+$file_handle = fopen('sharedmem.txt','w+');
+fwrite($file_handle,"Before exec");
+fclose($file_handle);
 $output = shell_exec($command);
 echo $output;
 
