9972a319f6c0bf9b608c6d0f606bc818df373e2b
NVRC <nathaniel.charlebois@gmail.com>
2017-09-23 17:23:03 -0400

    More testing

--- php/leds.php
+++ php/leds.php
@@ -1,16 +1,17 @@
 <?php
 
-    $command = escapeshellcmd('python led_output.py  123');
+    $command = escapeshellcmd('python /var/www/led-web-controller/python/led_output.py  123');
     $output = shell_exec($command);
     echo $output;
 
-
+    /*
     $colorArray = array();
     $NUM_LEDS = 60;
 
     for($i = 0; $i < $NUM_LEDS; $i++){
         $colorArray[] = $_POST[$i];
     }
+    */
 
 
 
