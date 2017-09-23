2ef48efc84c66a3d6e5563f30c00c85410f701f7
NVRC <nathaniel.charlebois@gmail.com>
2017-09-23 16:51:19 -0400

    Even more fixes

--- python/led_output.py
+++ python/led_output.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
-if __name__ == '__main__':
-    import sys
-    print(sys.argv[1:])
-    f = open('sharedmem.txt','w+')
-    f.write(sys.argv[1:][0])
-    f.close()
+import sys
+print(sys.argv[1:])
+f = open('sharedmem.txt','w+')
+f.write(sys.argv[1:][0])
+f.close()
