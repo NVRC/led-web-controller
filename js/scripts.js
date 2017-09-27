1881e6f1f337ca3fb54516668378528e534d92e0
NVRC <nathaniel.charlebois@gmail.com>
2017-09-27 18:15:52 -0400

    Finger on the pulse

--- js/scripts.js
+++ js/scripts.js
@@ -95,9 +95,9 @@ function changeColor(){
         console.log(formData);
 
         var request = new XMLHttpRequest();
-        request.open("GET","http://192.168.0.100/php/leds.php?"+formData,true);
+        request.open("GET",'http://192.168.0.100/php/leds.php?'+formData,true);
         //request.setRequestHeader("Content-Type", "application/json");
-        request.send(formData);
+        //request.send(formData);
 
         console.log('LED Form submitted');
     }
