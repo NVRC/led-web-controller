93be351262d92463a858e9771ae3be75925dba0f
NVRC <nathaniel.charlebois@gmail.com>
2017-09-29 21:51:36 -0400

    I thikn this mught do it

--- python/led_output.py
+++ python/led_output.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env
-import imp
+from support import linearGradientWrapper
 import sys
+
+
+
 if __name__ == '__main__':
     colors = sys.argv[1:]
+    
+    for(i = 0; i < 60; i++)
+        color_seg.append(colors[i:i+6])
+    print(color_seg)
 
-src = imp.load_source('linearGradientWrapper', '/home/pi/Adafruit_DotStar_Pi/linearGradientWrapper.py')
 
-lgw = src.linearGradientWrapper();
-lgw.addArray(colors)
-lgw.display()
+    lgw = linearGradientWrapper()
+    lgw.addArray(colors)
+    lgw.display()
