2502daf42390f3d45601521dc8debbf85beb1684
NVRC <nathaniel.charlebois@gmail.com>
2017-09-29 21:56:39 -0400

    I thikn this mught do it

--- python/led_output.py
+++ python/led_output.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
     colors = sys.argv[1:]
 
-    for i in range(0,60)
+    for i in range(0,60):
         color_seg.append(colors[i:i+6])
         i=i+6
     print(color_seg)
