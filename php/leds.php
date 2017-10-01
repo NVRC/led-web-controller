75cfbdb1792a04a28230e7605649e6cc32887695
NVRC <nathaniel.charlebois@gmail.com>
2017-10-01 12:17:06 -0400

    Updated php

--- php/leds.php
+++ php/leds.php
@@ -1,30 +1,37 @@
 <?php
 
-    //Node.js would be a good alternative to this tedious php workflow
-    $colors = array();
-    $python = '../python/';
-
-    $currDir = getcwd();
-    error_log($currDir);
-
-    header("Content-type: text/plain");
-
-
-    if(isset($_GET)){
-        echo ":: data received via GET ::\n\n";
-        foreach ($_GET as $key => $value)
-            $colors[$key] = ltrim($value,'#');
-
-        $colorString = implode("",$colors);
-        chdir($python);
-        $command = escapeshellcmd('sudo python led_output.py '.$colorString);
-        $output = shell_exec($command);
-
-        chdir($currDir);
-        error_log("SUCCESS: ".$output);
+    if(ini_get('safe_mode')){
+        echo "Safe mode is on \n Current user: ".get_current_user();
     } else {
-        echo "No array of colors sent!";
+        echo "Current user:  ".get_current_user();
+        $colors = array();
+        $python = '../python/';
+
+        $currDir = getcwd();
+        error_log($currDir);
+
+        header("Content-type: text/plain");
+
+
+        if(isset($_GET)){
+            echo ":: data received via GET ::\n\n";
+            foreach ($_GET as $key => $value)
+                $colors[$key] = ltrim($value,'#');
+
+            $colorString = implode("",$colors);
+            chdir($python);
+            $command = escapeshellcmd('sudo python led_output.py '.$colorString);
+            $output = shell_exec($command);
+
+            chdir($currDir);
+            error_log("SUCCESS: ".$output);
+        } else {
+            echo "No array of colors sent!";
+        }
     }
+
+    //Node.js would be a good alternative to this tedious php workflow
+
 
 
 
