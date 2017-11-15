4298d7ad68cc740d1284e6a08dec8a428e5e4d58
NVRC <nathaniel.charlebois@gmail.com>
2017-11-15 18:44:08 -0500

    Improved python

--- python/led_output.py
+++ python/led_output.py
@@ -10,10 +10,8 @@ if __name__ == '__main__':
     for i in range(0,60):
         color_seg.append(colors[i:i+6])
         i=i+6
-    print('COLOR SEG')
-    print(color_seg)
 
 
     lgw = linearGradientWrapper.linearGradientWrapper()
-    lgw.addArray(colors)
+    lgw.addArray(color_seg)
     lgw.display()
