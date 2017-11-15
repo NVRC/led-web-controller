0ebde2757269b9921fc427404d528ffa0db7a8db
NVRC <nathaniel.charlebois@gmail.com>
2017-11-15 18:47:14 -0500

    Improved python

--- python/led_output.py
+++ python/led_output.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     colors = sys.argv[1:]
     color_seg = []
     k=0;
-    for i in range(0,59):
-        color_seg[k](colors[i:i+6])
+    for i in range(0,60):
+        color_seg[k]=(colors[i:i+6])
         i=i+6
         k+=1
 
