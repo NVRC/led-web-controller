881a3552ee68a82d8b089cc133a50fcc4723c052
NVRC <nathaniel.charlebois@gmail.com>
2017-11-15 17:51:51 -0500

    Updated JS

--- js/scripts.js
+++ js/scripts.js
@@ -89,7 +89,7 @@ function changeColor(){
         for(i=0;i<NUM_LEDS-1;i++){
             formData += i+"="+colorArray[i]+"&";
         }
-        formData += NUM_LEDS-1+"="+colorArray[NUM_LEDS-1];
+        
 
         var URI = 'http://192.168.0.100/php/leds.php?'+formData;
 
